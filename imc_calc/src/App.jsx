116e581e5b86117b57764733f7c6bbc237dd5dc8
Danilo Amorim <daniloamorim@MacBook-Pro-de-Danilo.local>
2024-05-07 21:01:39 -0300

    primeiras configuracoes em react

---
+++ imc_calc/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ImcCalc from './components/ImcCalc'
+function App() {
+  return (
+      <div className='container'>
+     <h1>teste</h1>
+     <ImcCalc/>
+      </div>
+  )
+}
+
+export default App
